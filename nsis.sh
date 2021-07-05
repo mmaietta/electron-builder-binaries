ebf19288eb5f86cd35a0789210fffe2599089b09
Mike <mmaietta@outlook.com>
2021-07-05 11:36:50 -0700

    Recompiled 3.06.1 with `scons NSIS_CONFIG_LOG=yes NSIS_MAX_STRLEN=8192 PREFIX=/tmp/nsis install-compiler install-stubs`

--- nsis.sh
+++ nsis.sh
@@ -7,7 +7,7 @@ set -e
 # 1. Download source code from https://sourceforge.net/projects/nsis/files/NSIS%203/
 #   - Example: nsis-3.06.1-src.tar.bz2 - https://sourceforge.net/projects/nsis/files/NSIS%203/3.06.1/nsis-3.06.1-src.tar.bz2/download
 #   - Extract and enter directory.
-#   - `scons NSIS_CONFIG_LOG=yes NSIS_MAX_STRLEN=8192 PREFIX=C:\somewhere install-compiler install-stubs`
+#   - `scons NSIS_CONFIG_LOG=yes NSIS_MAX_STRLEN=8192 PREFIX=/tmp/nsis install-compiler install-stubs`
 #   - You may need to download https://nsis.sourceforge.io/Zlib 32bit. Provide via cmd line arg `ZLIB_W32=<absolute path>/Zlib-1.2.7-win32-x86`
 # 2. Copy over nsis in this repo and copy nsis-lang-fixes to nsis/Contrib/Language files
 # 3. Inspect changed and unversioned files — delete if need.
