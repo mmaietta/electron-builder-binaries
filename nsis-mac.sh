f1950824e03c280e708ddb939a48c5b901b9c0a9
Mike Maietta <mmaietta@outlook.com>
2025-04-12 17:12:47 -0700

    split up scripts

---
+++ nsis-mac.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -ex
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUT_DIR=/tmp/nsis
+rm -rf $OUTPUT_DIR
+mkdir $OUTPUT_DIR
+
+# Download the latest version of NSIS (macOS)
+mkdir -p $OUTPUT_DIR/mac
+brew tap nsis-dev/makensis
+brew install makensis@3.11 --with-large-strings --with-advanced-logging
+cp $(which makensis) $OUTPUT_DIR/mac/makensis
+
+cp -a $OUTPUT_DIR/* $BASEDIR/nsis
