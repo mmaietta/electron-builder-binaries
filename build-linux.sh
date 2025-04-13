aa08cab359f4d9cbc63373fee5ff7363bcb90c12
Mike Maietta <mmaietta@outlook.com>
2025-04-12 17:32:30 -0700

    set dir

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-sh appImage-packages-ia32.sh
-sh appImage-packages-x64.sh
-sh appimage-tools-x64.sh
-sh nsis-linux.sh
-sh nsis-plugins.sh
-sh nsis-prepare.sh
-sh update-zstd.sh
-sh winCodeSign-tools-x64.sh
+sh ./scripts/appImage-packages-ia32.sh
+sh ./scripts/appImage-packages-x64.sh
+sh ./scripts/appimage-tools-x64.sh
+sh ./scripts/nsis-linux.sh
+sh ./scripts/nsis-plugins.sh
+sh ./scripts/nsis-prepare.sh
+sh ./scripts/update-zstd.sh
+sh ./scripts/winCodeSign-tools-x64.sh
--- build-mac.sh
+++ build-mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -ex
 
-sh nsis-mac.sh
-sh wine/wine-mac-ia32-and-x64.sh
-sh scripts/update-zstd.sh
-sh ./winCodeSign/darwin/build.sh
+sh ./scripts/nsis-mac.sh
+sh ./scripts/wine/wine-mac-ia32-and-x64.sh
+sh ./scripts/scripts/update-zstd.sh
+sh ./scripts/./winCodeSign/darwin/build.sh
