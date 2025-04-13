3dfe70c7f68472a59996e9b5f9f3fc6bb3487413
Mike Maietta <mmaietta@outlook.com>
2025-04-12 17:50:34 -0700

    update linux script

--- build-linux.sh
+++ build-linux.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-sh ./scripts/appImage-packages-ia32.sh
-sh ./scripts/appImage-packages-x64.sh
-sh ./scripts/appimage-tools-x64.sh
 sh ./scripts/nsis-linux.sh
 sh ./scripts/nsis-plugins.sh
 sh ./scripts/nsis-prepare.sh
 sh ./scripts/update-zstd.sh
 sh ./scripts/winCodeSign-tools-x64.sh
+sh ./scripts/appimage-tools-x64.sh
+sh ./scripts/appImage-packages-ia32.sh
+sh ./scripts/appImage-packages-x64.sh
