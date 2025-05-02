59b3456f70872583b60299f4e720ab0be4a0a869
Mike Maietta <mmaietta@outlook.com>
2025-05-02 09:09:26 -0700

    fix ruby version

--- packages/fpm/version.sh
+++ packages/fpm/version.sh
@@ -1,2 +1,3 @@
-export RUBY_VERSION=$(ruby --version | grep -oE '([0-9]+\.){2}[0-9]+' | head -n 1)
+# extract ruby version but without the patch version
+export RUBY_VERSION="$(ruby --version | grep -oE '([0-9]+\.){1}[0-9]+' | head -n 1).0"
 export FPM_VERSION=1.16.0
