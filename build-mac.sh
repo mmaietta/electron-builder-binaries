01cd962663a9c349b939628da63eafea3bba25d4
Mike Maietta <mmaietta@outlook.com>
2025-05-03 10:34:01 -0700

    re-enable mac builds

--- build-mac.sh
+++ build-mac.sh
@@ -2,10 +2,10 @@
 set -ex
 
 bash ./packages/fpm/fpm.sh fpm/darwin/fpm.7z
-# sh ./scripts/linux-tools-mac.sh
-# sh ./scripts/nsis-mac.sh
-# sh ./scripts/zstd-win-mac.sh
-# sh ./scripts/nsis-plugins-TBD.sh
-# sh ./scripts/appimage-mac.sh
-# sh ./packages/win-codesign/darwin/build.sh
-# sh ./packages/wine/wine-mac-ia32-and-x64.sh
+sh ./scripts/linux-tools-mac.sh
+sh ./scripts/nsis-mac.sh
+sh ./scripts/zstd-win-mac.sh
+sh ./scripts/nsis-plugins-TBD.sh
+sh ./scripts/appimage-mac.sh
+sh ./packages/win-codesign/darwin/build.sh
+sh ./packages/wine/wine-mac-ia32-and-x64.sh
