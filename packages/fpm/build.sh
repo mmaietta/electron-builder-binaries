c300ef464dabffaf7f88a9bcd4df56511722eb3e
Mike Maietta <mmaietta@outlook.com>
2025-05-06 15:27:19 -0700

    tmp save

--- packages/fpm/build.sh
+++ packages/fpm/build.sh
@@ -30,12 +30,12 @@ echo "FPM_VERSION: $FPM_VERSION"
 
 # --------------------------------------------------------
 # rm -rf $TMP_DIR/ruby/$RUBY_VERSION/{build_info,cache,doc,extensions,doc,plugins,specifications,tests}
-echo "Fpm: $FPM_VERSION" >$TMP_DIR/VERSION.txt
-echo "Ruby: $RUBY_VERSION" >>$TMP_DIR/VERSION.txt
+echo "Fpm: $FPM_VERSION" > $TMP_DIR/VERSION.txt
+echo "Ruby: $RUBY_VERSION" >> $TMP_DIR/VERSION.txt
 
 # copy ruby interpreter and libraries
 RUBY_BIN="$(which ruby)"
-GEM_HOME="/tmp/.ruby_bundle_gems"
+# GEM_HOME="/tmp/.ruby_bundle_gems"
 if [ "$(uname)" = "Darwin" ]; then
     # # MacOS
     # cp -a $(otool -L $RUBY_EXEC | grep -E 'libssl|libcrypto|libruby' | awk '{print $1}') $INTERPRETER/bin/
@@ -58,6 +58,14 @@ if [ "$(uname)" = "Darwin" ]; then
     echo "[+] Copying Ruby installation..."
     cp -a "$RUBY_PREFIX" "$BUNDLE_DIR/ruby"
 
+    echo "[+] Copying Ruby gems..."
+    mkdir -p "$BUNDLE_DIR/ruby/bin.real"
+    GEMS="bundle bundler gem irb rake ruby"
+    for bin in $GEMS; do
+        echo "  [COPY] $bin"
+        cp -aL "$(which $bin)" "$BUNDLE_DIR/ruby/bin.real/$bin"
+    done
+
     # === EXTRACT SHARED LIB DEPENDENCIES ===
     echo "[+] Scanning Ruby binary for dynamic libraries..."
     mkdir -p "$BUNDLE_DIR/lib"
@@ -72,32 +80,25 @@ if [ "$(uname)" = "Darwin" ]; then
     done
 
     # === PATCH BINARY PATHS ===
-    echo "[+] Patching Ruby binary install names..."
+    # echo "[+] Patching Ruby binary install names..."
     # cd "$BUNDLE_DIR/ruby/bin"
-    for bin in ruby irb gem; do
-        [[ -x "$bin" ]] || continue
-        # otool -L "$bin" | grep -v ":" | awk '{print $1}' | while read lib; do
-        #     if [[ "$lib" == @* || "$lib" == /usr/* || "$lib" == /System/* ]]; then
-        #         continue
-        #     fi
-        #     libname=$(basename "$lib")
-        #     echo "  [REWRITE] $lib -> @loader_path/../lib/$libname"
-        #     install_name_tool -change "$lib" "@loader_path/../lib/$libname" "$bin"
-        # done
-        install_name_tool -add_rpath @executable_path/../lib $BUNDLE_DIR/ruby/bin.real/$bin
-    done
+    # for cmd in ruby irb gem; do
+    #     bin="$BUNDLE_DIR/ruby/bin.real/$cmd"
+    #     [[ -x "$bin" ]] || continue
+    #     otool -L "$bin" | grep -v ":" | awk '{print $1}' | while read lib; do
+    #         if [[ "$lib" == @* || "$lib" == /usr/* || "$lib" == /System/* ]]; then
+    #             continue
+    #         fi
+    #         libname=$(basename "$lib")
+    #         echo "  [REWRITE] $lib -> @loader_path/../lib/$libname"
+    #         # install_name_tool -change "$lib" "@loader_path/../lib/$libname" "$bin"
+    #     done
+    #     install_name_tool -add_rpath @executable_path/../lib $bin
+    # done
     # cd ../../..
 
-    mkdir -p "$BUNDLE_DIR/ruby/bin.real"
-    GEMS="bundle bundler gem irb rake ruby"
-    for bin in $GEMS; do
-        echo "  [COPY] $bin"
-        cp -aL "$(which $bin)" "$BUNDLE_DIR/ruby/bin.real/"
-        # cp -avf "$(readlink -f "$(which $bin)")" "$BUNDLE_DIR/ruby/bin.real/"
-        install_name_tool -add_rpath @executable_path/../lib $BUNDLE_DIR/ruby/bin.real/$bin
-    done
     # for bin in ruby irb gem; do
-        # install_name_tool -add_rpath @executable_path/../lib $BUNDLE_DIR/ruby/bin.real/$bin
+    # install_name_tool -add_rpath @executable_path/../lib $BUNDLE_DIR/ruby/bin.real/$bin
     # done
 else
     # Linux
@@ -113,16 +114,17 @@ else
 
     # === COPY RUBY BINARY ===
     echo "[+] Copying Ruby binary..."
-    GEM_COMMAND="gem install bundle bundler --no-document --quiet"
-    $GEM_COMMAND || sudo $GEM_COMMAND
-
+    cp -a "$RUBY_REAL_BIN" "$BUNDLE_DIR/bin.real/ruby"
+    
     echo "[+] Copying Ruby gems..."
-    GEMS="bundle bundler gem irb rake ruby"
-    for bin in $GEMS; do
+    GEMS="bundle bundler irb puma rake redcarpet thin unicorn"
+    GEM_COMMAND="gem install $GEMS --no-document --quiet"
+    $GEM_COMMAND || sudo $GEM_COMMAND
+    for bin in gem $GEMS; do
         echo "  [COPY] $bin"
-        cp -aL "$(which $bin)" "$BUNDLE_DIR/bin.real/"
-        # cp -avf "$(readlink -f "$(which $bin)")" "$BUNDLE_DIR/bin.real/"
+        cp -aL "$(which $bin)" "$BUNDLE_DIR/bin.real/$bin"
     done
+
     echo "[+] Patching RPATH to use bundled lib/"
     patchelf --set-rpath '$ORIGIN/../lib' "$BUNDLE_DIR/bin.real/ruby"
 
@@ -157,20 +159,22 @@ GIT_REPO_DIR=$TMP_DIR/lib/app
 mkdir -p $GIT_REPO_DIR
 cp -a $BASEDIR/packages/fpm/node_modules/fpm/{bin,lib,misc,templates} $GIT_REPO_DIR
 
-mkdir -p $TMP_DIR/lib/vendor/lib/fpm
-cp -a $BASEDIR/packages/fpm/assets/.bundle $TMP_DIR/lib/vendor/
-cp -a $BASEDIR/packages/fpm/node_modules/fpm/{Gemfile*,fpm.gemspec} $TMP_DIR/lib/vendor/
-cp -a $BASEDIR/packages/fpm/node_modules/fpm/lib/fpm/version.rb $TMP_DIR/lib/vendor/lib/fpm/version.rb
+VENDOR_DIR=$TMP_DIR/lib/vendor
+mkdir -p $VENDOR_DIR/lib/fpm
+cp -a $BASEDIR/packages/fpm/assets/.bundle $VENDOR_DIR/
+cp -a $BASEDIR/packages/fpm/node_modules/fpm/{Gemfile*,fpm.gemspec} $VENDOR_DIR/
+cp -a $BASEDIR/packages/fpm/node_modules/fpm/lib/fpm/version.rb $VENDOR_DIR/lib/fpm/version.rb
 
-export GEM_HOME
+# export GEM_HOME
 gem install bundle bundler --no-document --quiet || sudo gem install bundler --no-document --quiet
-cd $TMP_DIR/lib/vendor
+cd "$VENDOR_DIR"
 bundle install
-rm -rf "$TMP_DIR/lib/vendor/**/cache"
+rm -rf "$VENDOR_DIR/**/cache"
 
 # create entry scripts
 ENTRY_SCRIPT=$TMP_DIR/fpm
-cat >$ENTRY_SCRIPT <<'EOL'
+echo "  [WRITE] fpm entrypoint -> $ENTRY_SCRIPT"
+cat > $ENTRY_SCRIPT <<'EOL'
 #!/bin/bash
 set -e
 
@@ -193,19 +197,19 @@ for bin in gem irb rake; do
     ENTRY_SCRIPT="$BUNDLE_DIR/bin/$bin"
     echo "  [COPY] $bin -> $ENTRY_SCRIPT"
     cp "$BASEDIR/packages/fpm/assets/entrypoint.sh" $ENTRY_SCRIPT
-    echo "exec "\$ROOT/bin.real/ruby" "\$ROOT/bin.real/$bin" "\$@"" >>$ENTRY_SCRIPT
+    echo "exec "\$ROOT/bin.real/ruby" "\$ROOT/bin.real/$bin" "\$@"" >> $ENTRY_SCRIPT
     chmod +x $ENTRY_SCRIPT
 done
 
 ENTRY_SCRIPT=$BUNDLE_DIR/bin/ruby_environment
 echo "  [COPY] ruby_environment -> $ENTRY_SCRIPT"
-cat $BASEDIR/packages/fpm/assets/ruby_environment | sed "s|RUBY_VERSION|$RUBY_VERSION|g" >$ENTRY_SCRIPT
+cat $BASEDIR/packages/fpm/assets/ruby_environment | sed "s|RUBY_VERSION|$RUBY_VERSION|g" > $ENTRY_SCRIPT
 chmod +x $ENTRY_SCRIPT
 
 ENTRY_SCRIPT=$BUNDLE_DIR/bin/ruby
 echo "  [COPY] ruby entrypoint -> $ENTRY_SCRIPT"
-cat $BASEDIR/packages/fpm/assets/entrypoint.sh >$ENTRY_SCRIPT
-echo "exec "\$ROOT/bin.real/ruby" "\$@"" >>$ENTRY_SCRIPT
+cp "$BASEDIR/packages/fpm/assets/entrypoint.sh" $ENTRY_SCRIPT
+echo "exec "\$ROOT/bin.real/ruby" "\$@"" >> $ENTRY_SCRIPT
 chmod +x $ENTRY_SCRIPT
 
 ENTRY_SCRIPT=$BUNDLE_DIR/lib/restore_environment.rb
