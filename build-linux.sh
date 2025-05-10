75413f3f5ba2bf1a0ce44aa15fe7614dce0536d8
Mike Maietta <mmaietta@outlook.com>
2025-05-09 20:28:20 -0700

    tmp save

--- build-linux.sh
+++ build-linux.sh
@@ -4,5 +4,5 @@ set -e
 CWD=$(cd "$(dirname "$0")" && pwd)
 
 export ARCH=${ARCH:-$(uname -m)}
-export OS_TARGET=${OS_TARGET:-$(uname | tr '[:upper:]' '[:lower:]')}
+export OS_TARGET="linux"
 bash "$CWD/packages/fpm/build.sh"
