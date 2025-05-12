32dea7d2ccde47445490c714d9f125effd09b63f
Mike Maietta <mmaietta@outlook.com>
2025-05-11 20:15:04 -0700

    ignore again

--- scripts/changeset-version.js
+++ scripts/changeset-version.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const fs = require("fs");
-const { execSync } = require("child_process");
+const { execSync, exec } = require("child_process");
 
 // changeset status expects relative __dirname even if we set absolute output path
 const changesetJsonPath = "changeset-status.json";
@@ -12,6 +12,23 @@ const releases = changesetJson.releases;
 
 console.log("Release candidates:", releases);
 
+const shouldDelete = (filePath) => {
+  const isReleaseCandidate = releases.some((release) => {
+    const artifactPath = path.resolve(__dirname, "../artifacts", release.name);
+    return filePath.startsWith(artifactPath);
+  });
+  return !isReleaseCandidate;
+};
+fs.readdirSync(path.resolve(__dirname, "../artifacts")).forEach((file) => {
+  const filePath = path.resolve(__dirname, "../artifacts", file);
+  if (shouldDelete(filePath)) {
+    console.log(`Deleting ${filePath}...`);
+    if (!process.env.DRY_RUN) {
+      execSync(`git rm --force -r ${filePath}`);
+    }
+    return;
+  }
+})
 releases.forEach((release) => {
   const { name } = release;
   const artifactPath = path.resolve(__dirname, "../artifacts", name);
