09cc18f51fc6c1bbdd509349cbc8f62a87032be9
Mike Maietta <mmaietta@outlook.com>
2025-05-17 07:34:19 -0700

    back to bash?

--- packages/squirrel.windows/build-squirrel.ps1
+++ packages/squirrel.windows/build-squirrel.ps1
@@ -27,14 +27,14 @@ Get-ChildItem -Recurse -Filter *.csproj -Path $repoRoot | ForEach-Object {
   (Get-Content $_.FullName) -replace 'v4\.5', 'v4.5.2' | Set-Content $_.FullName
 }
 
-# # Retarget C++ toolset and Windows SDK
-# Get-ChildItem -Recurse -Filter *.vcxproj -Path $repoRoot | ForEach-Object {
-#   $file = $_.FullName
-#   $content = Get-Content $file
-#   $content = $content -replace '<PlatformToolset>v141</PlatformToolset>', '<PlatformToolset>v143</PlatformToolset>'
-# #   $content = $content -replace '<WindowsTargetPlatformVersion>8.1</WindowsTargetPlatformVersion>', '<WindowsTargetPlatformVersion>10.0.19041.0</WindowsTargetPlatformVersion>'
-#   Set-Content $file $content
-# }
+# Retarget C++ toolset and Windows SDK
+Get-ChildItem -Recurse -Filter *.vcxproj -Path $repoRoot | ForEach-Object {
+  $file = $_.FullName
+  $content = Get-Content $file
+  $content = $content -replace '<PlatformToolset>v141</PlatformToolset>', '<PlatformToolset>v143</PlatformToolset>'
+#   $content = $content -replace '<WindowsTargetPlatformVersion>8.1</WindowsTargetPlatformVersion>', '<WindowsTargetPlatformVersion>10.0.19041.0</WindowsTargetPlatformVersion>'
+  Set-Content $file $content
+}
 
 # Install Visual Studio 2017 Build Tools with v141 toolset
 $vsInstallerUrl = "https://aka.ms/vs/15/release/vs_buildtools.exe"
--- packages/squirrel.windows/build.sh
+++ packages/squirrel.windows/build.sh
@@ -3,6 +3,11 @@ set -ex
 
 VERSION=2.0.1
 
+BASE_DIR=$(cd "$(dirname "$BASH_SOURCE")" && pwd)
+
+REPO_URL="https://github.com/Squirrel/Squirrel.Windows"
+NUGET_EXE="https://dist.nuget.org/win-x86-commandline/latest/nuget.exe"
+
 export DOTNET_NOLOGO=true
 export DOTNET_SKIP_FIRST_TIME_EXPERIENCE=true
 export DOTNET_CLI_TELEMETRY_OPTOUT=true
@@ -17,15 +22,38 @@ mkdir -p $OUT_DIR
 TMP_DIR=/tmp/squirrel
 rm -rf $TMP_DIR
 mkdir $TMP_DIR
-
 cd $TMP_DIR
-git clone --single-branch --depth 1 --branch $VERSION --recursive https://github.com/squirrel/squirrel.windows
-cd $TMP_DIR/squirrel.windows
-cp -a $BASEDIR/patches/* $TMP_DIR/squirrel.windows
-git apply $TMP_DIR/squirrel.windows/*.patch
 
+git clone --single-branch --depth 1 --branch $VERSION --recursive https://github.com/squirrel/squirrel.windows
+
+SRC_DIR=$TMP_DIR/squirrel.windows
+cd $SRC_DIR
+cp -a $BASEDIR/patches/* $SRC_DIR
+git apply $SRC_DIR/*.patch
+
+# echo "Downloading NuGet..."
+# curl -L -o nuget.exe "$NUGET_EXE"
+
+echo "Retargeting .csproj files to .NET Framework 4.5.2..."
+find . -name "*.csproj" -exec sed -i 's|<TargetFrameworkVersion>v4.5|<TargetFrameworkVersion>v4.5.2|g' {} +
+
+echo "Retargeting .vcxproj files to PlatformToolset v143..."
+find . -name "*.vcxproj" -exec sed -i 's|<PlatformToolset>v141|<PlatformToolset>v143|g' {} +
+
+# echo "Restoring NuGet packages..."
+# ./nuget.exe restore Squirrel.sln
 ./.nuget/NuGet.exe restore
-msbuild /p:Configuration=Release
+
+echo "Building solution with MSBuild..."
+MSBUILD_PATH="/c/Program Files/Microsoft Visual Studio/2022/Enterprise/MSBuild/Current/Bin/MSBuild.exe"
+if [ ! -f "$MSBUILD_PATH" ]; then
+  MSBUILD_PATH="/c/Program Files/Microsoft Visual Studio/2022/Community/MSBuild/Current/Bin/MSBuild.exe"
+fi
+
+"$MSBUILD_PATH" Squirrel.sln /p:Configuration=Release /m
+
+# ./.nuget/NuGet.exe restore
+# msbuild /p:Configuration=Release
 
 echo $VERSION > $TMP_DIR/VERSION.txt
 
