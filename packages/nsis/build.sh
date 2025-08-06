9fe52cc071c423ce6bb5676176c84145ea6688e6
Mike Maietta <mmaietta@outlook.com>
2025-08-06 09:55:54 -0700

    fix windows/linux script

--- packages/nsis/build.sh
+++ packages/nsis/build.sh
@@ -9,14 +9,14 @@ OS_TARGET=${OS_TARGET:-$(uname | tr '[:upper:]' '[:lower:]')}
 
 if [ "$OS_TARGET" = "darwin" ]; then
     echo "Building for macOS"
-    sh "$CWD/assets/nsis-mac.sh"
+    bash "$CWD/assets/nsis-mac.sh"
 elif [ "$OS_TARGET" = "linux" ]; then
     echo "Building for Linux"
-    sh "$CWD/assets/nsis-windows.sh"
+    bash "$CWD/assets/nsis-windows.sh"
 elif [ "$OS_TARGET" = "win32" ] || [ "$OS_TARGET" = "windows" ]; then
     echo "Building for Windows"
     # sh "$CWD/assets/nsis-prepare.sh"
-    sh "$CWD/assets/nsis-windows.sh"
+    bash "$CWD/assets/nsis-windows.sh"
 else
     echo "Building for Linux and Windows"
     bash "$CWD/assets/nsis-windows.sh"
